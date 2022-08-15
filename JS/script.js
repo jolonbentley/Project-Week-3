bde5c4197d9f5ce25d40774b09ab009ed1b0c39f
Jolon Bentley <jolon88@hotmail.com>
2022-08-15 15:43:47 +1200

    Tidied up code and added the script file

---
+++ JS/script.js
@@ -0,0 +1,46 @@
+function changeMD() {
+
+    var ceoImage = document.getElementById("ceoImage");
+    var ceoHeading = document.getElementById("ceoHeading");
+    var ceoParagraph = document.getElementById("ceoParagraph");
+    var ceoButton = document.getElementById("ceoButton");
+
+    if (ceoHeading.textContent == "Floyd Mayweather") {
+        ceoHeading.innerHTML = "Thomas Bradley";
+        ceoParagraph.innerHTML = "Managing Director";
+        ceoImage.src = "/images/md.jpg";
+        ceoButton.innerHTML = "Change to CEO";
+    }
+    else {
+        ceoHeading.innerHTML = "Floyd Mayweather";
+        ceoParagraph.innerHTML = "Chief Executive Officer";
+        ceoImage.src = "/Images/ceo-about.jpg";
+        ceoButton.innerHTML = "Change to MD";
+    }
+
+}
+
+/*Created to toggle between the two styles when clicked*/
+
+function contactHeading() {
+    var toggleHeading = document.getElementById("contactHeading");
+    toggleHeading.classList.toggle("contactHeadingAltStyle");
+}
+
+/*Created to mouseover content and shift it to the right*/
+
+var btn = document.getElementById("staffBtn");
+btn.style.visibility = "hidden";
+
+function hideAllStaff() {
+    var staff = document.getElementById("allStaff");
+    staff.style.visibility = "hidden"
+}
+
+function showStaffBtn() {
+    btn.style.visibility = "visible";
+}
+
+function hideStaffBtn() {
+    btn.style.visibility = "hidden"
+}
